f7d0bfb4411b1011c9080f5d130765b6d33df53c
Douglas G. Allen <kb9agt@gmail.com>
2015-02-09 13:23:38 -0600

    Update crt_time.rb

--- crt_time.rb
+++ crt_time.rb
@@ -1,4 +1,5 @@
 # windows version
+# https://msdn.microsoft.com/en-us/library/634ca0c2%28v=vs.80%29.aspx
 
 require 'ffi'
  
@@ -12,3 +13,5 @@ tbuffer = ""
 puts CPUTime._strtime(tbuffer) 
 
 # https://github.com/ffi/ffi
+# my current libc.so.6 does not contain this function
+# FFI::Library::LIBC.methods.sort - Object.methods
