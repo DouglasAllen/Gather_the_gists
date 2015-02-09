94e521ed7d3efe19315c0e8b13a461e5419549ad
Douglas G. Allen <kb9agt@gmail.com>
2015-02-09 11:03:12 -0600

    Update suntimes.rb

--- suntimes.rb
+++ suntimes.rb
@@ -20,10 +20,10 @@ eot.jd = @date.jd
 for i in 0..50
   eot.date = (@date + i).to_s
   jd = eot.jd + i
-  puts "Sunrise #{eot.display_time(eot.sunrise_dt, 0)}"
-  puts "Sunrise #{eot.display_time(eot.sunrise_time(jd), @zone)}"
-  puts "Sunset #{eot.display_time(eot.sunset_time(jd), 0)}"
+  puts "Sunrise #{eot.sunrise_dt}"
+  puts "Sunrise #{eot.sunrise_dt.to_time.utc}"
+  puts "Sunset #{eot.sunset_dt}"
   # reset the date after this else it increments twice if sunset time is over 24.
   eot.date = (@date + i).to_s
-  puts "Sunset #{eot.display_time(eot.sunset_time(jd), @zone)}"
+  puts "Sunset #{eot.sunset_dt.to_time.utc}"
 end 
